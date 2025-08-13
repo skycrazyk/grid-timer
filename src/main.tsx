71e82b6208c6a73878f9e928cd6cf1ef5b0d6610
Dmitry Karyakin <skycrazyk@gmail.com>
2025-08-13 21:45:24 +0200

    Hello 5 hours later

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import App from "./App.tsx"
 
+// biome-ignore lint/style/noNonNullAssertion: root always exist
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
 		<App />
